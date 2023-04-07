52172518ebcb4b62844e833c9ff717f5768aaa19
Metehan <metehan.bolat.ie@gmail.com>
2023-04-08 02:23:43 +0300

    * Sample app has been created.

--- app/src/main/java/com/metehanbolat/endtoendtestcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/endtoendtestcompose/MainActivity.kt
@@ -3,13 +3,6 @@ package com.metehanbolat.endtoendtestcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.metehanbolat.endtoendtestcompose.ui.theme.EndToEndTestComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EndToEndTestComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EndToEndTestComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/metehanbolat/endtoendtestcompose/ThirdScreen.kt
@@ -0,0 +1,29 @@
+package com.metehanbolat.endtoendtestcompose
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ThirdScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(15.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.third_screen_description),
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
+}
